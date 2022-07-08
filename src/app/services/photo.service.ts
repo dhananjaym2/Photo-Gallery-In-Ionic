b1a71c5695292cf6ea718c5456aed3daaf977f98
Dhananjay Mohnot <dhananjaymohnot@gmail.com>
2022-07-08 08:27:12 +0530

    Import Capacitor dependencies and get references to the Camera, Filesystem, and Storage plugins.

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
 
 @Injectable({
   providedIn: 'root'
