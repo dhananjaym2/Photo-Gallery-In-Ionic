6d5bf830885c96bc33937afa926c2afa1a508243
Dhananjay Mohnot <dhananjaymohnot@gmail.com>
2022-07-08 08:42:08 +0530

    Define a new class method, addNewToGallery, that will contain the core logic to take a device photo and save it to the filesystem. Let’s start by opening the device camera.

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -9,4 +9,13 @@ import { Storage } from '@capacitor/storage';
 export class PhotoService {
 
   constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    })
+  }
 }
